4753ff828a4914c89c7aa84666c2d83b3210ab94
JoeGravelle <jogravelle@gmail.com>
2014-01-10 22:45:06 -0500

    Second Init
    Basic movement, jumping, and physics worked out

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Variables
+
+	//Floats
+	public float maxSpeed;
+	public float jumpForce;
+	public float groundCheckRadius;
+	float movementSpeed;
+	//Booleans
+	bool grounded = true;
+	//Other
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//If the player is on the ground and they press the Spacebar, they will jump.
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) 
+		{
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
+	void FixedUpdate() {
+		//Player Movement
+
+		//Controls horizontal movement
+		movementSpeed = Input.GetAxis ("Horizontal");
+		//Checks to see if the player is currently on the ground.
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+		//Applies the velocity to the player.
+		rigidbody2D.velocity = new Vector2 (movementSpeed * maxSpeed, rigidbody2D.velocity.y);
+	}
+
+}
